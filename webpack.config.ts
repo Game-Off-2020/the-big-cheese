d745c7e10288bd90d71564e69b531d65cbd88f66
Barnabás BARTHA <barnabas@barthaweb.com>
2020-11-08 19:16:13 +0100

    Reformat webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,26 +1,26 @@
-import * as path from "path";
-import OptimizeCssAssetsPlugin from "optimize-css-assets-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import HtmlWebPackPlugin from "html-webpack-plugin";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import * as path from 'path';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import { Configuration, HotModuleReplacementPlugin } from 'webpack';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import OptimizeJsPlugin from "optimize-js-plugin";
-import CopyWebpackPlugin from "copy-webpack-plugin";
+import OptimizeJsPlugin from 'optimize-js-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import ReplaceInFileWebpackPlugin from "replace-in-file-webpack-plugin";
-import TerserPlugin from "terser-webpack-plugin";
+import ReplaceInFileWebpackPlugin from 'replace-in-file-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import GeneratePackageJsonPlugin from "generate-package-json-webpack-plugin";
+import GeneratePackageJsonPlugin from 'generate-package-json-webpack-plugin';
 
 interface Target {
    readonly entry: string;
    readonly distDir: string;
    readonly output: string;
-   readonly target: "web" | "node";
+   readonly target: 'web' | 'node';
    readonly title?: string;
    readonly assetDir?: string;
    readonly distAssetDir?: string;
@@ -29,51 +29,51 @@ interface Target {
 
 const targets: { client: Target; server: Target } = {
    client: {
-      entry: "src/main/client/client.main.ts",
-      distDir: "dist/client",
-      output: "main.js",
-      target: "web",
-      title: "Game Off 2020",
-      assetDir: "src/asset",
-      distAssetDir: "asset",
-      baseUrl: "."
+      entry: 'src/main/client/client.main.ts',
+      distDir: 'dist/client',
+      output: 'main.js',
+      target: 'web',
+      title: 'Game Off 2020',
+      assetDir: 'src/asset',
+      distAssetDir: 'asset',
+      baseUrl: '.',
    },
    server: {
-      entry: "src/main/server/server.main.ts",
-      distDir: "dist/server",
-      output: "main.js",
-      target: "node"
-   }
+      entry: 'src/main/server/server.main.ts',
+      distDir: 'dist/server',
+      output: 'main.js',
+      target: 'node',
+   },
 };
 
 module.exports = (env: string, argv: { [key: string]: string }): Configuration => {
    function isProd(): boolean {
-      return argv.mode === "production";
+      return argv.mode === 'production';
    }
 
-   const targetName = (argv.projectTarget || Object.keys(targets)[0]) as "client" | "server";
+   const targetName = (argv.projectTarget || Object.keys(targets)[0]) as 'client' | 'server';
    const target = targets[targetName];
-   console.log("Target:", targetName, target);
-   console.log("Entry: ", path.resolve(__dirname, target.entry));
-   console.log("Output path: ", path.resolve(__dirname, target.distDir));
+   console.log('Target:', targetName, target);
+   console.log('Entry: ', path.resolve(__dirname, target.entry));
+   console.log('Output path: ', path.resolve(__dirname, target.distDir));
 
    /*
     * Plugins
     */
    const plugins = [
       new MiniCssExtractPlugin({
-         filename: "[name].bundle.css",
-         chunkFilename: "[id].css"
-      })
+         filename: '[name].bundle.css',
+         chunkFilename: '[id].css',
+      }),
    ];
-   if (isProd() && (targetName === "client" || targetName === "server")) {
+   if (isProd() && (targetName === 'client' || targetName === 'server')) {
       plugins.push(
          new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, target.distDir + "/**/*")]
-         })
+            cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, target.distDir + '/**/*')],
+         }),
       );
    }
-   if (targetName === "client") {
+   if (targetName === 'client') {
       // plugins.push(new ThreadsPlugin()); // TODO: Enable when using workers for the network
       if (isProd()) {
          plugins.push(
@@ -83,50 +83,50 @@ module.exports = (env: string, argv: { [key: string]: string }): Configuration =
                   files: [target.output],
                   rules: [
                      {
-                        search: ".worker.js",
-                        replace: ".worker.js?" + Date.now()
-                     }
-                  ]
-               }
+                        search: '.worker.js',
+                        replace: '.worker.js?' + Date.now(),
+                     },
+                  ],
+               },
             ]),
             new ReplaceInFileWebpackPlugin([
                {
                   dir: target.distDir,
-                  files: ["index.html"],
+                  files: ['index.html'],
                   rules: [
                      {
                         // Avoid caching after build
                         search: target.output,
-                        replace: `${target.output}?${Date.now()}`
-                     }
+                        replace: `${target.output}?${Date.now()}`,
+                     },
                      // {
                      //    search: `<meta name="base" content="${target.baseUrl}">`,
                      //    replace: `<meta name="base" content="${target.baseUrl}">\n<meta http-equiv="cache-control" content="no-cache" />`
                      // }
-                  ]
-               }
+                  ],
+               },
             ]),
-            new CopyWebpackPlugin({ patterns: [{ from: target.assetDir, to: target.distAssetDir }] })
+            new CopyWebpackPlugin({ patterns: [{ from: target.assetDir, to: target.distAssetDir }] }),
          );
       }
       plugins.push(
          new HtmlWebPackPlugin({
-            filename: "index.html",
+            filename: 'index.html',
             title: target.title,
             meta: {
-               base: "/",
-               viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
-               "theme-color": "#000000"
-            }
-         })
+               base: '/',
+               viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+               'theme-color': '#000000',
+            },
+         }),
       );
    } else {
       plugins.push(
          new GeneratePackageJsonPlugin({
             scripts: {
-               start: "node main.js"
-            }
-         })
+               start: 'node main.js',
+            },
+         }),
       );
    }
    if (!isProd()) {
@@ -138,16 +138,16 @@ module.exports = (env: string, argv: { [key: string]: string }): Configuration =
     */
    const minimizers = [
       new OptimizeJsPlugin({
-         sourceMap: !isProd()
+         sourceMap: !isProd(),
       }),
       new OptimizeCssAssetsPlugin({
          cssProcessorPluginOptions: {
-            preset: ["default", { discardComments: { removeAll: true } }]
-         }
-      })
+            preset: ['default', { discardComments: { removeAll: true } }],
+         },
+      }),
    ];
 
-   if (isProd() && target.target === "web") {
+   if (isProd() && target.target === 'web') {
       minimizers.push(
          new TerserPlugin({
             parallel: false,
@@ -156,10 +156,10 @@ module.exports = (env: string, argv: { [key: string]: string }): Configuration =
                keep_classnames: false,
                keep_fnames: false,
                compress: {
-                  drop_console: true
-               }
-            }
-         })
+                  drop_console: true,
+               },
+            },
+         }),
       );
    }
 
@@ -169,7 +169,7 @@ module.exports = (env: string, argv: { [key: string]: string }): Configuration =
       output: {
          path: path.resolve(__dirname, target.distDir),
          filename: target.output,
-         pathinfo: false
+         pathinfo: false,
       },
       target: target.target,
       //      externals: target.target === "node" ? [
@@ -178,89 +178,89 @@ module.exports = (env: string, argv: { [key: string]: string }): Configuration =
       //         })
       //      ] : [],
       devServer: {
-         port: 8081
+         port: 8081,
       },
       resolve: {
          extensions: [
-            ".tsx",
-            ...(() => (isProd() ? [".prod.ts"] : []))(), // This will override prod config
-            ".ts",
-            ".js"
-         ]
+            '.tsx',
+            ...(() => (isProd() ? ['.prod.ts'] : []))(), // This will override prod config
+            '.ts',
+            '.js',
+         ],
       },
-      devtool: !isProd() ? "inline-source-map" : false,
+      devtool: !isProd() ? 'inline-source-map' : false,
       module: {
          rules: [
             {
                test: /\.css$/,
-               use: ["style-loader", "css-loader"]
+               use: ['style-loader', 'css-loader'],
             },
             {
                test: /\.scss$/,
-               use: ["style-loader", "css-loader", "sass-loader"]
+               use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                test: /\.tsx?$/,
                use: [
                   {
-                     loader: "babel-loader"
+                     loader: 'babel-loader',
                   },
                   {
-                     loader: "ts-loader",
+                     loader: 'ts-loader',
                      options: {
                         transpileOnly: true, // Disabled due to performance reasons
                         experimentalWatchApi: !isProd(),
                         compilerOptions: {
-                           module: "esnext"
-                        }
-                     }
-                  }
-               ]
+                           module: 'esnext',
+                        },
+                     },
+                  },
+               ],
             },
             {
                test: /\.html$/,
                use: [
                   {
-                     loader: "html-loader",
+                     loader: 'html-loader',
                      options: {
-                        minimize: true
-                     }
-                  }
-               ]
+                        minimize: true,
+                     },
+                  },
+               ],
             },
             {
                test: /\.(png|jpg|jpeg|gif|svg|bin|glb|gltf|woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
                use: [
                   {
-                     loader: "file-loader",
+                     loader: 'file-loader',
                      options: {
-                        name: "[path]/[name].[ext]",
+                        name: '[path]/[name].[ext]',
                         outputPath: (url: string, resourcePath: string, context: string) => {
                            if (isProd()) {
                               // Remove "src/" from asset imports in prod. It is necessary only locally
-                              return path.relative(context, resourcePath).replace(path.normalize("src/"), "");
+                              return path.relative(context, resourcePath).replace(path.normalize('src/'), '');
                            } else {
                               return url;
                            }
-                        }
-                     }
-                  }
-               ]
+                        },
+                     },
+                  },
+               ],
             },
             {
                test: /\.(gltf)$/,
                use: [
                   {
-                     loader: "gltf-webpack-loader"
-                  }
-               ]
-            }
-         ]
+                     loader: 'gltf-webpack-loader',
+                  },
+               ],
+            },
+         ],
       },
       optimization: {
          minimize: isProd(),
-         minimizer: minimizers
+         minimizer: minimizers,
       },
-      plugins: plugins
+      plugins: plugins,
    };
 };
