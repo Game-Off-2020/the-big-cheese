4079075b1976abfe5f0951b48061db01061e69a4
Dolan Miu <dolan_miu@hotmail.com>
2020-11-17 20:01:41 +0000

    Use simple vector as well as Phaser's vector

--- src/main/shared/bullet/bullet-model.ts
+++ src/main/shared/bullet/bullet-model.ts
@@ -1,6 +1,12 @@
 import * as Phaser from 'phaser';
+
+export interface SimpleVector {
+   readonly x: number;
+   readonly y: number;
+}
+
 export interface Bullet {
    playerId: string;
-   readonly position: Phaser.Math.Vector2;
-   readonly direction: Phaser.Math.Vector2;
+   readonly position: Phaser.Math.Vector2 | SimpleVector;
+   readonly direction: Phaser.Math.Vector2 | SimpleVector;
 }
