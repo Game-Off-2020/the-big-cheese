a7e79a6f7b3f256ff4f4198a76645d55824b40dd
Barnabás BARTHA <barnabas@barthaweb.com>
2020-11-21 19:03:10 +0100

    Change scale

--- src/main/shared/config/common-shared-config.ts
+++ src/main/shared/config/common-shared-config.ts
@@ -2,5 +2,5 @@ export class CommonSharedConfig {
    static readonly NETWORK_TICK_RATE: number = 8; // Max data sync per sec
    static readonly SERVER_PORT: number = 3001;
    static readonly BULLET_BASE_SPEED: number = (70 * 1000) / 60;
-   static readonly MAP_OUTPUT_SCALE: number = 6; // The real canvas will be this time smaller than what we see
+   static readonly MAP_OUTPUT_SCALE: number = 4; // The real canvas will be this time smaller than what we see
 }
