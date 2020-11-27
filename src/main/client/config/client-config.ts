76d66ce97a5d3cb002fdc7e5e94437f657e62c0b
Barnabás BARTHA <barnabas@barthaweb.com>
2020-11-27 13:48:42 +0100

    Prod port 3000

--- src/main/client/config/client-config.ts
+++ src/main/client/config/client-config.ts
@@ -1,7 +1,13 @@
-import { CommonClientConfig } from './common-client-config';
+import { CommonClientConfig, ServerHost } from './common-client-config';
 
 // Import this class for config properties, though, you should define here only development properties
 export class ClientConfig extends CommonClientConfig {
    static readonly ASSET_FOLDER: string = '../../src/asset/';
    static readonly AUTO_START: boolean = false; // Skip main menu for development purposes
+   static readonly SERVER_HOSTS: ServerHost[] = [
+      {
+         url: `http://localhost:${CommonClientConfig.SERVER_SOCKET_PORT}`,
+         name: 'Localhost',
+      },
+   ];
 }
