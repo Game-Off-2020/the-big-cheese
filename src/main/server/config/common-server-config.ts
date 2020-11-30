3fcf99a41f936718e5c93ab40cb80bcef9ef979d
Barnabás BARTHA <barnabas@barthaweb.com>
2020-11-30 18:02:47 +0100

    Decrease dropped cheeses on hit

--- src/main/server/config/common-server-config.ts
+++ src/main/server/config/common-server-config.ts
@@ -4,8 +4,8 @@ export class CommonServerConfig extends SharedConfig {
    static readonly MOON_RADIUS: number = 600 * CommonServerConfig.MAP_OUTPUT_SCALE;
    static readonly TICK_RATE: number = CommonServerConfig.NETWORK_TICK_RATE;
    static readonly BULLET_SPEED: number = CommonServerConfig.BULLET_BASE_SPEED / CommonServerConfig.TICK_RATE;
-   static readonly DROP_CHEESE_PERCENTAGE: number = 0.15;
-   static readonly MAX_DROP_CHEESE: number = 15;
+   static readonly DROP_CHEESE_PERCENTAGE: number = 0.08;
+   static readonly MAX_DROP_CHEESE: number = 10;
    static readonly CHEESE_BOMB_COUNT: number = 15;
    static readonly BULLET_BLAST_RADIUS_MIN: number = 50;
    static readonly BULLET_BLAST_RADIUS_MAX: number = 60;
